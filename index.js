92093c550ca36c7e9e74da0b9412f1e8ed012dfe
AlifianMuhammad <alifianmuhammad.business@gmail.com>
2022-12-05 08:57:52 +0700

    add cors and morgan

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const dbClient = require("./db");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.port || 9000;
+
+app.use(cors());
 
 app.use(express.json());
 
