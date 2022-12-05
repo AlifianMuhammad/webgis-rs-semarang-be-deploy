dffd71f149bf8d6013dd524d3d39f356069edbdc
AlifianMuhammad <alifianmuhammad.business@gmail.com>
2022-12-05 08:45:16 +0700

    add new endpoint

--- index.js
+++ index.js
@@ -6,14 +6,18 @@ const port = process.env.port || 9000;
 
 app.use(express.json());
 
-app.get("/todos", async (req, res) => {
-  const result = await dbClient.query("SELECT * FROM todos");
+app.get("/api/v1/rumahsakit", async (req, res) => {
+  const result = await dbClient.query("SELECT * FROM rumah_sakit");
   res.status(200).json(result.rows);
 });
 
-app.get("/rs", async (req, res) => {
-  const result = await dbClient.query("SELECT smid, nama FROM rumah_sakit");
-  res.status(200).json(result.rows);
+app.get("/api/v1/rumahsakit/all", async (req, res) => {
+  const result = await dbClient.query(
+    "SELECT nama as label, smid as value  FROM rumah_sakit"
+  );
+  res.status(200).json({
+    rumahsakit: result.rows,
+  });
 });
 
 app.listen(port, () => console.log(`listen on port ${port}`));
