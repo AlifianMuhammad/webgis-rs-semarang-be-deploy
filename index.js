7f0cddffe951322f4bdfb43fd5eb6993b9685f17
AlifianMuhammad <alifianmuhammad.business@gmail.com>
2022-12-05 09:52:21 +0700

    renew api

--- index.js
+++ index.js
@@ -23,5 +23,21 @@ app.get("/rumahsakit", async (req, res) => {
     rumahsakit: result.rows,
   });
 });
+app.get("/spesialislabelkey", async (req, res) => {
+  const result = await dbClient.query(
+    "SELECT nama_spesialis as label, nama_spesialis as key FROM spesialis where id<5"
+  );
+  res.status(200).json({
+    spesialislabelkey: result.rows,
+  });
+});
+app.get("/spesialislabelvalue", async (req, res) => {
+  const result = await dbClient.query(
+    "SELECT nama_spesialis as label, nama_spesialis as value FROM spesialis where id<5"
+  );
+  res.status(200).json({
+    spesialislabelvalue: result.rows,
+  });
+});
 
 app.listen(port, () => console.log(`listen on port ${port}`));
