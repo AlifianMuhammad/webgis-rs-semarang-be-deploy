2b9b1db18ce961f4166f01e24910b00b7122e1e9
AlifianMuhammad <alifianmuhammad.business@gmail.com>
2022-12-05 09:18:07 +0700

    all item

--- index.js
+++ index.js
@@ -23,18 +23,18 @@ app.get("/api/v1/rumahsakit/all", async (req, res) => {
     rumahsakit: result.rows,
   });
 });
+app.get("/api/v1/rumahsakit/allitem", async (req, res) => {
+  const result = await dbClient.query(
+    "SELECT nama as label, smid as value  FROM rumah_sakit"
+  );
+  res.status(200).json({
+    rumahsakit: result.rows,
+  });
+});
 
 app.get("api/v1/spesialis/all", async (req, res) => {
   const result = await dbClient.query(
     "SELECT nama_spesialis as label, nama_spesialis as key FROM spesialis WHERE id<15"
-  );
-  res.status(200).json({
-    spesialisAll: result.rows,
-  });
-});
-app.get("api/v1/spesialis/allvalue", async (req, res) => {
-  const result = await dbClient.query(
-    "SELECT nama_spesialis as label, nama_spesialis as value FROM spesialis WHERE id<15"
   );
   res.status(200).json({
     spesialisAll: result.rows,
